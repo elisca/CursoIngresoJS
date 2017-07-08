bd3f1770d8aae88cc648c371d349c54d0485bd8a
elisca <ezequiel.lisca@live.com>
2017-07-08 16:18:20 -0300

    Adivina el Numero 1 y 2
    Completos y funcionales

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -8,19 +8,38 @@ secreto del 1 al 100, en la pantalla del juego
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
 var numeroSecreto; 
-var contadorIntentos;
+var numUsuario=0;
+var contadorIntentos=0;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+	numeroSecreto=Math.floor(1+(Math.random()*100));
+	console.log("NS: "+numeroSecreto);
 
+	document.getElementById('numero').value=0;
+	document.getElementById('intentos').value=contadorIntentos;
 }
 
 function verificar()
 {
+		numUsuario=document.getElementById('numero').value;
 	
-	
+		if(numUsuario==numeroSecreto)
+		{
+			alert("Usted es un ganador!!! Y solo en "+contadorIntentos+" intentos");
+			alert("Cliquee COMENZAR para adivinar un nuevo numero");
+			contadorIntentos=0;
+		}
+		else if(numUsuario<numeroSecreto)
+		{
+			alert("Falta para llegar al numero secreto");
+			contadorIntentos++;
+		}
+		else if(numUsuario>numeroSecreto)
+		{
+			alert("Te pasaste para llegar al numero secreto");			
+			contadorIntentos++;
+		}
+
+		document.getElementById('intentos').value=contadorIntentos;
 }
--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -9,20 +9,69 @@
 Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
+//Definimos variables
 var numeroSecreto; 
 var contadorIntentos;
+var numeroUsuario;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
+	//Genera numero aleatorio entre 1 y 100, truncado con Math Floor
+	numeroSecreto=Math.floor(1+Math.random()*100);
+	console.log("NS: "+numeroSecreto);
 
+	//Inicializa en cero las veces que cliqueamos verificar
+	contadorIntentos=0;
+	document.getElementById('intentos').value=contadorIntentos;
 }
 
 function verificar()
 {
-	
-	
+	//Tomo el numero ingresado por el usuario
+	numeroUsuario=document.getElementById('numero').value;
 
+	//Aumenta la cantidad de intentos realizados en 1
+	contadorIntentos++;
+	document.getElementById('intentos').value=contadorIntentos;
+
+	//Compara si adivinamos el numero, o en otro caso, si falta o nos pasamos
+	if(numeroUsuario<numeroSecreto)
+	{
+		alert("Falta para llegar al numero secreto");
+	}
+	else if(numeroUsuario>numeroSecreto)
+	{
+		alert("Te pasaste para llegar al numero secreto");
+	}
+	else if(numeroUsuario==numeroSecreto)
+	{
+		//Si adivinamos el numero, muestra cartel segun intentos
+		switch(contadorIntentos)
+		{
+			case 1:
+				alert("Usted es un psiquico");
+				break;
+			case 2:
+				alert("Excelente percepcion");
+				break;
+			case 3:
+				alert("Esto es suerte");
+				break;
+			case 4:
+				alert("Excelente tecnica");
+				break;
+			case 5:
+				alert("Usted esta en la media");
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				alert("Falta tecnica");
+				break;
+			default:
+				alert("Afortunado en el amor");
+		}
+	}
 }
